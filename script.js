351d5fba591489a1e19f81961f529aa3666838f6
Manoj Kumar M <142601120+MANOJ-KUMAR-M-247@users.noreply.github.com>
2024-07-23 23:42:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+//select side-navbar,Navbar-menu
+var sidenav=document.querySelector(".side-navbar")
+
+function shownavbar(){
+    sidenav.style.left="0%";
+}
+function closenavbar(){
+    sidenav.style.left="-60%";
+}
